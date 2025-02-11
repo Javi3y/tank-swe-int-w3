7db8af33a7e07ed4df9bfb8cfc50177c9728e14c
javi3y <javieyprograms@gmail.com>
2025-02-11 10:34:39 +0330

    added permissions to client

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.adapters.postgres import get_db
 from app.adapters.redis import run_redis
-from app.auth.service.entrypoints import auth
+from app.auth.entrypoints import auth
 from app.users.adapters.mappers import (
     #    author_mapper,
     client_mapper,
