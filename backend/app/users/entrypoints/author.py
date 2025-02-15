fe174efb697c8faabf23716e3da2e9a5ecb02f3f
javi3y <javieyprograms@gmail.com>
2025-02-15 13:31:04 +0330

    author

--- backend/app/users/entrypoints/author.py
+++ backend/app/users/entrypoints/author.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 
 from app.unit_of_work import UnitOfWork
 from app.users.domain.entities.author import AuthorOut
-from app.users.service.author import AuthorService
+from app.users.service.query.author import get_author_query, get_authors_query
 
 
 router = APIRouter(prefix="/authors", tags=["Authors"])
@@ -12,8 +12,7 @@ router = APIRouter(prefix="/authors", tags=["Authors"])
 @router.get("/", response_model=List[AuthorOut])
 async def get_authors():
     async with UnitOfWork() as uow:
-        author_service = AuthorService()
-        return await author_service.get_items(uow)
+        return await get_authors_query(uow)
 
 
 @router.get("/{id}", response_model=AuthorOut)
@@ -21,5 +20,4 @@ async def get_author(
     id: int,
 ):
     async with UnitOfWork() as uow:
-        author_service = AuthorService()
-        return await author_service.get_item(id, uow)
+        return await get_author_query(id, uow)
